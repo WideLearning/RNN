76945bac8e8c7f2bc5c8e6f66043d1c024bb1d3a
msh2481 <msh2481@mail.ru>
2023-01-13 11:46:12 +0100

    Added generation to RNN example

--- example.py
+++ example.py
@@ -1,4 +1,7 @@
+import numpy as np
 import torch
+import torch.nn.functional as F
+from tqdm import tqdm
 
 from data import ptb_loaders
 from model import RNN
@@ -7,7 +10,7 @@ from trainer import TeacherForcingTrainer
 
 dl_train, dl_val = ptb_loaders(
     n_seq=10,
-    n_batch=512,
+    n_batch=1024,
 )
 net = RNN(
     n_x=128,
@@ -22,4 +25,27 @@ trainer = TeacherForcingTrainer(
     tracker=ConsoleTracker(k=1, regex=".*/loss"),
 )
 
-trainer.train(n_epochs=3)
+
+def to_onehot(c):
+    return F.one_hot(torch.tensor([c]), num_classes=128).float()
+
+
+for epoch in range(100):
+    trainer.train(n_epochs=1)
+    torch.save(net.state_dict(), "rnn.tmp")
+
+    with torch.no_grad():
+        print(f"Epoch #{epoch}")
+        prompt = "The quick brown"
+        onehot = [to_onehot(ord(c)) for c in prompt]
+        state = net.init_state(std=0, n_batch=1)
+        logprobs = None
+        for x in onehot:
+            logprobs, state = net(x, state)
+        for it in range(100):
+            probs = logprobs.exp().numpy().ravel()
+            c = np.random.choice(128, p=probs)
+            prompt += chr(c)
+            onehot.append(to_onehot(c))
+            logprobs, state = net(x, state)
+        print(prompt)
